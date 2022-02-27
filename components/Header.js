0e39fd542d9e74cced9179957802fc28f8860a98
Jay Siraskar <siraskarjay17@gmail.com>
2022-02-27 22:44:40 +0530

    final changes

--- components/Header.js
+++ components/Header.js
@@ -22,7 +22,7 @@ function Header() {
                         <Avatar logoutOnPress/> 
                     </div>
 
-                    <h1 className="text-3xl">Welcome to PAPAFAM Metaverse</h1>
+                    <h1 className="text-3xl">Welcome to Jay Siraskar's Metaverse</h1>
                     <h2 className="text-5xl font-bold truncate"> {user.
                         getUsername()}</h2>
 
