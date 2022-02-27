ba507aedf80c7cf943655e78f878e203c39e6ae9
Jay Siraskar <siraskarjay17@gmail.com>
2022-02-27 16:58:40 +0530

    Metaverse first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
+
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: ["avatars.dicebars.com", "links.papareact.com"],
+  },
+};
---
+++ pages/index.js
@@ -0,0 +1,28 @@
+
+import Head from 'next/head'
+import { useMoralis } from 'react-moralis';
+import Login from '../components/Login';
+
+
+
+export default function Home() {
+
+  const {isAuthenticated, logout} = useMoralis();
+
+
+
+  if(!isAuthenticated) return <Login/>;
+  return (
+    <div className="h-screen">
+      <Head>
+        <title>Metaverse</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+<h1>Welcome to the APP</h1>
+
+  <button onClick={logout}>Logout</button>
+    </div>
+  )
+}
+
+
